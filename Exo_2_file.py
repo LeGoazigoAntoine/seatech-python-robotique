3c4cc3a2dacfd6d569bc4376c4ec3fb892752cb5
LeGoazigoAntoine <antoine.legoazigo@gmail.com~>
2023-01-09 12:02:34 +0100

    Exo 2

--- Exo_2_file.py
+++ Exo_2_file.py
@@ -3,19 +3,26 @@ from Exo1_file import Robot
 from Exo2_Human import Human
 
 class Cyborg(Robot, Human):   
-    pass
-# ---------------------------------------------
-# Main
-# ---------------------------------------------
-if __name__ =='__main__':
-    cyborg = Cyborg('Deux Ex Machina', 'M')
+    def __init__(self, name, sexe):
+        Robot.__init__(self, name)
+        Human.__init__(self, sexe)
+        self.name = name
+        self.sexe = sexe
 
-    print(cyborg.name, 'sexe', cyborg.sexe)
-    print('Charging battery...')
-    cyborg.charge()
-    cyborg.status()
-    cyborg.eat('banana')
-    cyborg.eat(['coca', 'chips'])
-    cyborg.digest()
-    cyborg.changePayloadStatus()
-    del cyborg
+    def resume_c(self):
+        print("\nVoici un resume du Cyborg : \n")
+        print("Le Cyborg s'appelle %s"%(cyborg.name))
+        print("Le Cyborg est %s"%(cyborg.current_state))
+        print("\n")
+
+######### Main  #########
+
+cyborg = Cyborg('Deux Ex Machina', 'M')
+
+print(cyborg.name, 'sexe', cyborg.sexe)
+print('Charging battery...')
+cyborg.charging()
+cyborg.eat('banana')
+cyborg.eat(['coca', 'chips'])
+cyborg.digest()
+cyborg.resume_c()
