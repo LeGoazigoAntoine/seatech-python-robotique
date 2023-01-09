3276134d5b4168ed091cdfe036dc13ef48354793
LeGoazigoAntoine <antoine.legoazigo@gmail.com~>
2023-01-09 10:09:41 +0100

    Exo2

--- Exo_2_file.py
+++ Exo_2_file.py
@@ -1,65 +1,41 @@
-class Robot():
-    name = ""
-    power = False
-    #__current_speed = 0
-    battery_level = 0
-    current_state = ''
-    current_speed = 0
-    states = ['shutown', 'running']
-    
-    def __init__(self, name=None):
-        self.name = name
-        self.power = True
-        self.current_state = self.states[1]
-
-    def off(self):
-        self.power = False
-        self.current_state = self.states[0]
-
-    def charging(self):
-        i = 0
-        while i < 100:
-            self.battery_level += 1
-            time.sleep(0.1)
-            i += 1
-            print(self.battery_level)
-
-    def move(self,speed):	
-        if type(speed) == int:
-            self.current_speed = speed
-
-    def stop(self):
-        self.current_speed = 0
-	
-    def speed(self):
-      return self.current_speed
-
-    def resume(self):
-        print("\nVoici un resume du robot : \n")
-        print("Le Robot s'appelle %s"%(r.name))
-        print("Le Robot est %s"%(r.current_state))
-        print("Le niveau de la batterie est de " + f'{r.battery_level}'+ " %.")
-        print("Vitesse du robot est de " + f'{r.speed()}' + " cm/s")
-        print("\n")
-
-
-class Human():   
-    # Human class content here
-    pass
+from random import randint
+from Exo1_file import Robot
+from Exo2_Human import Human
 
 class Cyborg(Robot, Human):   
-
-    def __init__(self, name, sexe):   
+    __nuke = False
+    def __init__(self, name, sexe, nuke=False):   
         Robot.__init__(self, name)
         Human.__init__(self, sexe)
+        self.__nuke = nuke
+        self.name = name
+        self.sexe = sexe
+    def __del__(self):
+        if self.__nuke == True:
+            print("AUTODESTRUCTION COMPLETE")
+            print("CASUALTIES: "+ str(randint(1000,10000000)))
+        else:
+            print("NUKE PAYLOAD HASN'T BEEN FOUND, AUTODESTRUCTION CANCELED")
+    def charge(self):
+        Robot.chargeBattery(self)
 
+    def status(self):
+        print(Robot.__str__(self))
+        print(Human.__str__(self))
+    def changePayloadStatus(self):
+        self.__nuke = not(self.__nuke)
+# ---------------------------------------------
+# Main
+# ---------------------------------------------
+if __name__ =='__main__':
+    cyborg = Cyborg('Deux Ex Machina', 'M')
 
-cyborg = Cyborg('Deux Ex Machina', 'M')
-
-print(cyborg.name, 'sexe', cyborg.sexe)
-print('Charging battery...')
-cyborg.charge()
-cyborg.status()
-cyborg.eat('banana')
-cyborg.eat(['coca', 'chips'])
-cyborg.digest()
+    print(cyborg.name, 'sexe', cyborg.sexe)
+    print('Charging battery...')
+    cyborg.charge()
+    cyborg.status()
+    cyborg.eat('banana')
+    cyborg.eat(['coca', 'chips'])
+    cyborg.digest()
+    cyborg.changePayloadStatus()
+    del cyborg
